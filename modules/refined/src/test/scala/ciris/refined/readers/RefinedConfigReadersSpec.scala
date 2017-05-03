7fd666bde8eeef96a58e773df519a22fe60ffeba
Viktor Lövgren <viktor.lovgren@ovoenergy.com>
2017-05-03 01:01:35 +0100

    Improve refined tests

--- modules/refined/src/test/scala/ciris/refined/readers/RefinedConfigReadersSpec.scala
+++ modules/refined/src/test/scala/ciris/refined/readers/RefinedConfigReadersSpec.scala
@@ -3,16 +3,21 @@ package ciris.refined.readers
 import ciris.PropertySpec
 import ciris.refined._
 import eu.timepit.refined.types.numeric.PosInt
+import org.scalacheck.Gen
 
 final class RefinedConfigReadersSpec extends PropertySpec {
   "RefinedConfigReaders" when {
     "reading a RefType" should {
       "successfully read RefType values" in {
-        readValue[PosInt]("1").right.map(_.value) shouldBe Right(1)
+        forAll(Gen.posNum[Int]) { posInt ⇒
+          readValue[PosInt](posInt.toString).right.map(_.value) shouldBe Right(posInt)
+        }
       }
 
       "return a failure for wrong type" in {
-        readValue[PosInt]("0") shouldBe a[Left[_, _]]
+        forAll(Gen.choose(Int.MinValue, 0)) { nonPosInt ⇒
+          readValue[PosInt](nonPosInt.toString) shouldBe a[Left[_, _]]
+        }
       }
 
       "return a failure for missing key" in {
