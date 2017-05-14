6a4b777d1c25b464faffb5962d791a7bfba6a8cc
Viktor Lövgren <viktor.lovgren@ovoenergy.com>
2017-05-14 18:30:20 +0100

    Add support java.io.File

--- modules/core/jvm/src/main/scala/ciris/readers/ConfigReaders.scala
+++ modules/core/jvm/src/main/scala/ciris/readers/ConfigReaders.scala
@@ -3,6 +3,7 @@ package ciris.readers
 trait ConfigReaders
     extends DerivedConfigReaders
     with DurationConfigReaders
+    with JavaIoConfigReaders
     with JavaNetConfigReaders
     with JavaNioCharsetConfigReaders
     with JavaNioFileConfigReaders
