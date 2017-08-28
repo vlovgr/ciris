dc8e855f9e3f8824283b47223b145fb06806e6d1
Viktor Lövgren <viktor.lovgren@ovoenergy.com>
2017-08-28 09:05:03 +0100

    Update scripts to latest version

--- scripts/try.sh
+++ scripts/try.sh
@@ -6,10 +6,11 @@ test -e ~/.coursier/coursier || ( \
 )
 
 ~/.coursier/coursier launch -q -P \
-  com.lihaoyi:ammonite_2.12.3:1.0.1 \
-  is.cir:ciris-core_2.12:0.4.0 \
-  is.cir:ciris-enumeratum_2.12:0.4.0 \
-  is.cir:ciris-generic_2.12:0.4.0 \
-  is.cir:ciris-refined_2.12:0.4.0 \
-  is.cir:ciris-squants_2.12:0.4.0 \
-  -- --predef-code 'import ciris._,ciris.enumeratum._,ciris.generic._,ciris.refined._,ciris.squants._' < /dev/tty
+  com.lihaoyi:ammonite_2.12.3:1.0.2 \
+  is.cir:ciris-core_2.12:0.4.1 \
+  is.cir:ciris-enumeratum_2.12:0.4.1 \
+  is.cir:ciris-generic_2.12:0.4.1 \
+  is.cir:ciris-refined_2.12:0.4.1 \
+  is.cir:ciris-spire_2.12:0.4.1 \
+  is.cir:ciris-squants_2.12:0.4.1 \
+  -- --predef-code 'import ciris._,ciris.enumeratum._,ciris.generic._,ciris.refined._,ciris.spire._,ciris.squants._' < /dev/tty
