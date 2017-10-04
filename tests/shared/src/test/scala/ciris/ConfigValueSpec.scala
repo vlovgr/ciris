1b525e431379ddb07b746779b409dce5437d7f5e
Viktor Lövgren <viktor.lovgren@ovoenergy.com>
2017-10-04 15:29:20 +0100

    Add ConfigValue#orElse tests

--- tests/shared/src/test/scala/ciris/ConfigValueSpec.scala
+++ tests/shared/src/test/scala/ciris/ConfigValueSpec.scala
@@ -7,5 +7,32 @@ final class ConfigValueSpec extends PropertySpec {
         readConfigValue[String]("value").toString shouldBe "ConfigValue(Right(value))"
       }
     }
+
+    "using orElse" should {
+      "use the first value even if the second is available" in {
+        val result = ConfigValue(Right(123)) orElse ConfigValue(Right(456))
+        result.value shouldBe Right(123)
+      }
+
+      "not evaluate the second value if the first is available" in {
+        ConfigValue(Right(123)) orElse fail("orElse evaluated second value")
+      }
+
+      "use the first value if the second one contains an error" in {
+        val result = ConfigValue(Right(123)) orElse ConfigValue(Left(ConfigError("error")))
+        result.value shouldBe Right(123)
+      }
+
+      "use the second value if first contains an error" in {
+        val result = ConfigValue(Left(ConfigError("error"))) orElse ConfigValue(Right(456))
+        result.value shouldBe Right(456)
+      }
+
+      "combine errors if both contain errors" in {
+        val (error1, error2) = (ConfigError("error1"), ConfigError("error2"))
+        val result = ConfigValue(Left(error1)) orElse ConfigValue(Left(error2))
+        result.value shouldBe Left(ConfigError.combined(error1, error2))
+      }
+    }
   }
 }
