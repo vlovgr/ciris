8cd86ab6d730184ed68953f798048dd41be53f93
Viktor Lövgren <viktor.lovgren@ovoenergy.com>
2017-10-13 13:19:50 +0100

    Add migration manager (MiMa) to build process

--- project/LatestVersion.scala
+++ project/LatestVersion.scala
@@ -9,12 +9,23 @@ object LatestVersion extends AutoPlugin {
     lazy val latestVersion: SettingKey[String] =
       settingKey[String]("Latest released version")
 
+    lazy val latestBinaryCompatibleVersion: SettingKey[Option[String]] =
+      settingKey[Option[String]]("Latest released binary-compatible version")
+
+    lazy val unreleasedModuleNames: SettingKey[Set[String]] =
+      settingKey[Set[String]]("Module names which have not yet been released")
+
     lazy val setLatestVersion: ReleaseStep = { state: State =>
       val extracted = Project.extract(state)
 
       val newLatestVersion = extracted.get(version in ThisBuild)
       val latestVersionFile = file("latestVersion.sbt")
-      val latestVersionFileContents = s"""latestVersion in ThisBuild := "$newLatestVersion"""" + "\n"
+      val latestVersionFileContents =
+        s"""
+          |latestVersion in ThisBuild := "$newLatestVersion"
+          |latestBinaryCompatibleVersion in ThisBuild := Some("$newLatestVersion")
+          |unreleasedModuleNames in ThisBuild := Set()
+         """.stripMargin.trim + "\n"
 
       IO.write(latestVersionFile, latestVersionFileContents)
       Vcs.detect(file(".")).foreach { vcs =>
