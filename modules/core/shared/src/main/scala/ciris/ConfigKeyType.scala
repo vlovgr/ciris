ba19df1f0889cb1ca3d701899b38ce3da3c21361
Viktor Lövgren <viktor.lovgren@ovoenergy.com>
2017-12-08 15:54:37 +0000

    Fix invalid documentation link [ci skip]

--- modules/core/shared/src/main/scala/ciris/ConfigKeyType.scala
+++ modules/core/shared/src/main/scala/ciris/ConfigKeyType.scala
@@ -8,7 +8,7 @@ package ciris
   * - [[ConfigKeyType#Environment]]: "environment variable" of type `String`,<br>
   * - [[ConfigKeyType#Property]]: "system property" of type `String`,<br>
   * - [[ConfigKeyType#Argument]]: "command-line argument" of type `Int`, and<br>
-  * - [[ConfigKeyType#File]]: "file" of type `(File, Charset)`.<br>
+  * - `ConfigKeyType#File`: "file" of type `(File, Charset)`.<br>
   * <br>
   * To create a new [[ConfigKeyType]] use the apply method in the companion object,
   * specifying the name of the key and its type. Note that there are no checks for
