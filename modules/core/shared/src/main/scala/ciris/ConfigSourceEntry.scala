ee538b1a80b4a241279ee29ff8a85abaa3c552ef
Viktor Lövgren <viktor.lovgren@ovoenergy.com>
2018-01-19 12:50:40 +0000

    Parameterize ConfigSourceEntry on the value

--- modules/core/shared/src/main/scala/ciris/ConfigSourceEntry.scala
+++ modules/core/shared/src/main/scala/ciris/ConfigSourceEntry.scala
@@ -3,26 +3,27 @@ package ciris
 /**
   * Represents an entry (key-value pair) that has been read from a configuration
   * source. The reading might have failed, represented by wrapping the value in
-  * an `Either[ConfigError, String]`. Values are always of type `String`, while
-  * the type `Key` can be different. Also includes the [[ConfigKeyType]] to
-  * be able to support better error messages.
+  * an `Either[ConfigError, Value]`. Keys are of type `Key` and values are of
+  * type `Value`. Also includes the [[ConfigKeyType]] to be able to support
+  * better error messages.
   *
-  * To create a [[ConfigSourceEntry]], use the [[ConfigSourceEntry#apply]] method.
+  * To create a [[ConfigSourceEntry]], use [[ConfigSourceEntry#apply]].
   *
   * {{{
   * scala> ConfigSourceEntry("key", ConfigKeyType.Environment, Right("value"))
-  * res0: ConfigSourceEntry[String] = ConfigSourceEntry(key, Environment, Right(value))
+  * res0: ConfigSourceEntry[String, String] = ConfigSourceEntry(key, Environment, Right(value))
   * }}}
   *
   * @param key the key which was read from the configuration source
   * @param keyType the type of keys the configuration source reads
   * @param value the value which was read or a [[ConfigError]]
-  * @tparam Key the type of key
+  * @tparam Key the type of the key
+  * @tparam Value the type of the value
   */
-sealed class ConfigSourceEntry[Key](
+sealed class ConfigSourceEntry[Key, Value](
   val key: Key,
   val keyType: ConfigKeyType[Key],
-  val value: Either[ConfigError, String]
+  val value: Either[ConfigError, Value]
 ) {
 
   /**
@@ -34,13 +35,13 @@ sealed class ConfigSourceEntry[Key](
     * @return a new [[ConfigSourceEntry]] with the transformed value
     * @example {{{
     * scala> val entry = ConfigSourceEntry("key", ConfigKeyType.Environment, Right("value "))
-    * entry: ConfigSourceEntry[String] = ConfigSourceEntry(key, Environment, Right(value ))
+    * entry: ConfigSourceEntry[String, String] = ConfigSourceEntry(key, Environment, Right(value ))
     *
     * scala> entry.mapValue(_.trim)
-    * res0: ConfigSourceEntry[String] = ConfigSourceEntry(key, Environment, Right(value))
+    * res0: ConfigSourceEntry[String, String] = ConfigSourceEntry(key, Environment, Right(value))
     * }}}
     */
-  def mapValue(f: String => String): ConfigSourceEntry[Key] =
+  def mapValue[A](f: Value => A): ConfigSourceEntry[Key, A] =
     new ConfigSourceEntry(key, keyType, value.right.map(f))
 
   override def toString: String =
@@ -63,14 +64,14 @@ object ConfigSourceEntry {
     * @return a new [[ConfigSourceEntry]] using the specified arguments
     * @example {{{
     * scala> ConfigSourceEntry("key", ConfigKeyType.Environment, Right("value"))
-    * res0: ConfigSourceEntry[String] = ConfigSourceEntry(key, Environment, Right(value))
+    * res0: ConfigSourceEntry[String, String] = ConfigSourceEntry(key, Environment, Right(value))
     * }}}
     */
-  def apply[Key](
+  def apply[Key, Value](
     key: Key,
     keyType: ConfigKeyType[Key],
-    value: Either[ConfigError, String]
-  ): ConfigSourceEntry[Key] = {
-    new ConfigSourceEntry[Key](key, keyType, value)
+    value: Either[ConfigError, Value]
+  ): ConfigSourceEntry[Key, Value] = {
+    new ConfigSourceEntry(key, keyType, value)
   }
 }
