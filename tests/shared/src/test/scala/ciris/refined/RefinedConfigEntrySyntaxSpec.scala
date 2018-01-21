2c8d407d25ed6b9ff1199647d92b1b4c87991600
Viktor Lövgren <viktor.lovgren@ovoenergy.com>
2018-01-21 14:32:10 +0000

    Change refined syntax to support ConfigEntry

--- tests/shared/src/test/scala/ciris/refined/RefinedConfigEntrySyntaxSpec.scala
+++ tests/shared/src/test/scala/ciris/refined/RefinedConfigEntrySyntaxSpec.scala
@@ -5,44 +5,49 @@ import ciris._
 import eu.timepit.refined.numeric.Positive
 import org.scalacheck.{Gen, Shrink}
 
-final class RefinedConfigValueSyntaxSpec extends PropertySpec {
-  implicit def noShrink[T] = Shrink[T](_ => Stream.empty)
+final class RefinedConfigEntrySyntaxSpec extends PropertySpec {
+  implicit def noShrink[T]: Shrink[T] = Shrink(_ => Stream.empty)
 
   "RefinedConfigValueSyntax" when {
     "using refine" should {
       "successfully refine values conforming to predicate" in {
         forAll(Gen.chooseNum(1, Int.MaxValue)) { n: Int =>
-          ConfigValue(Right(n)).refine[Positive].value shouldBe a[Right[_, _]]
+          ConfigEntry("key", ConfigKeyType.Property, Right(n))
+            .refineValue[Positive].value shouldBe a[Right[_, _]]
         }
       }
 
       "return an error when refining values not confirming to predicate" in {
         forAll(Gen.chooseNum(Int.MinValue, 0)) { n: Int =>
-          ConfigValue(Right(n)).refine[Positive].value shouldBe a[Left[_, _]]
+          ConfigEntry("key", ConfigKeyType.Property, Right(n))
+            .refineValue[Positive].value shouldBe a[Left[_, _]]
         }
       }
 
       "return an error when refining unavailable values" in {
-        ConfigValue[Int](Left(ConfigError("error"))).refine[Positive].value shouldBe a[Left[_, _]]
+        ConfigEntry[String, Int]("key", ConfigKeyType.Property, Left(ConfigError("error")))
+          .refineValue[Positive].value shouldBe a[Left[_, _]]
       }
     }
 
     "using mapRefine" should {
       "successfully refine value conforming to predicate" in {
         forAll(Gen.chooseNum(1, Int.MaxValue)) { n: Int =>
-          ConfigValue(Right(n)).mapRefine[Positive](identity).value shouldBe a[Right[_, _]]
+          ConfigEntry("key", ConfigKeyType.Property, Right(n))
+            .mapRefineValue[Positive](identity).value shouldBe a[Right[_, _]]
         }
       }
 
       "return an error when refining value not confirming to predicate" in {
         forAll(Gen.chooseNum(Int.MinValue, 0)) { n: Int =>
-          ConfigValue(Right(n)).mapRefine[Positive](identity).value shouldBe a[Left[_, _]]
+          ConfigEntry("key", ConfigKeyType.Property, Right(n))
+            .mapRefineValue[Positive](identity).value shouldBe a[Left[_, _]]
         }
       }
 
       "return an error when refining unavailable values" in {
-        ConfigValue[Int](Left(ConfigError("error")))
-          .mapRefine[Positive](identity)
+        ConfigEntry[String, Int]("key", ConfigKeyType.Property, Left(ConfigError("error")))
+          .mapRefineValue[Positive](identity)
           .value shouldBe a[Left[_, _]]
       }
     }
