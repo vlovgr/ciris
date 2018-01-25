5762b8c3a3926e43d2de60fc51ab2d7f61605fc8
Viktor Lövgren <viktor.lovgren@ovoenergy.com>
2018-01-25 11:24:51 +0000

    Add missing tests for new methods

--- tests/shared/src/test/scala/ciris/ConfigEntrySpec.scala
+++ tests/shared/src/test/scala/ciris/ConfigEntrySpec.scala
@@ -9,6 +9,16 @@ final class ConfigEntrySpec extends PropertySpec {
             s"ConfigEntry(key, ConfigKeyType(test key), Right($value))"
         }
       }
+
+      "include the sourceValue as well if different from the value" in {
+        forAll { value: String =>
+          existingEntry(value)
+            .mapValue(_ + "2")
+            .toString shouldBe {
+            s"ConfigEntry(key, ConfigKeyType(test key), Right($value), Right(${value}2))"
+          }
+        }
+      }
     }
 
     "using mapValue" when {
@@ -26,6 +36,32 @@ final class ConfigEntrySpec extends PropertySpec {
         "leave the value as it is" in {
           val entry = nonExistingEntry.mapValue(_.take(1))
           entry.value shouldBe nonExistingEntry.value
+        }
+      }
+    }
+
+    "using orElse" when {
+      "this value was read successfully" should {
+        "not attempt to use the other value" in {
+          existingEntry("value").orElse(fail("orElse"))
+        }
+      }
+
+      "this value was not read successfully" when {
+        "the other value was read successfully" should {
+          "use the other value" in {
+            nonExistingEntry
+              .orElse(existingEntry("value2"))
+              .value shouldBe Right("value2")
+          }
+        }
+
+        "the other value was not read successfully" should {
+          "accumulate the errors of both values" in {
+            val (first, second) = (nonExistingEntry, nonExistingEntry)
+            val (error1, error2) = (first.value.left.get, second.value.left.get)
+            first.orElse(second).value shouldBe Left(error1 combine error2)
+          }
         }
       }
     }
