51fee5a625bb04f8c9faaa3ed070f3a8e1acdd6b
Viktor Lövgren <viktor.lovgren@ovoenergy.com>
2018-02-05 15:30:30 +0000

    Add FunctionK#apply

--- modules/core/shared/src/main/scala/ciris/api/FunctionK.scala
+++ modules/core/shared/src/main/scala/ciris/api/FunctionK.scala
@@ -3,3 +3,7 @@ package ciris.api
 trait FunctionK[F[_], G[_]] {
   def apply[A](fa: F[A]): G[A]
 }
+
+object FunctionK {
+  def apply[F[_], G[_]](implicit fk: FunctionK[F, G]): FunctionK[F, G] = fk
+}
