ae355ee6f454665280a78e403b1dbc054586dc7f
Viktor Lövgren <viktor.lovgren@ovoenergy.com>
2018-02-12 08:04:14 +0000

    updated site

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,2 @@
 // Cleanup footer credits
 $('#site-footer .row:last p').html('Website built with <a href="https://47deg.github.io/sbt-microsites" target="_blank">sbt-microsites</a> by <a href="https://www.47deg.com" target="_blank">47 Degrees</a>')
-
-// Remove social icons
-$('.nav ul.pull-right li').slice(2).remove()
