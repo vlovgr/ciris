ca59315c933fee97b53e29a81c6598e86ca45ac2
Viktor Lövgren <viktor.lovgren@ovoenergy.com>
2018-02-12 07:42:30 +0000

    Update scripts to latest version

--- scripts/try.sh
+++ scripts/try.sh
@@ -7,15 +7,18 @@ test -e ~/.coursier/coursier || ( \
 
 ~/.coursier/coursier launch -q -P \
   com.lihaoyi:ammonite_2.12.4:1.0.3 \
-  is.cir:ciris-core_2.12:0.6.2 \
-  is.cir:ciris-enumeratum_2.12:0.6.2 \
-  is.cir:ciris-generic_2.12:0.6.2 \
-  is.cir:ciris-refined_2.12:0.6.2 \
-  is.cir:ciris-spire_2.12:0.6.2 \
-  is.cir:ciris-squants_2.12:0.6.2 \
+  is.cir:ciris-cats_2.12:0.7.0 \
+  is.cir:ciris-core_2.12:0.7.0 \
+  is.cir:ciris-enumeratum_2.12:0.7.0 \
+  is.cir:ciris-generic_2.12:0.7.0 \
+  is.cir:ciris-refined_2.12:0.7.0 \
+  is.cir:ciris-spire_2.12:0.7.0 \
+  is.cir:ciris-squants_2.12:0.7.0 \
   -- --predef-code "\
+        repl.compiler.settings.YpartialUnification.value = true;\
         import ciris._,\
         ciris.syntax._,\
+        ciris.cats._,\
         ciris.enumeratum._,\
         ciris.generic._,\
         ciris.refined._,\
