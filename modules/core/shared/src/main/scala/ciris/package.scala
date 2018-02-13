82ecd640a903db3ee00b68dbdf8bd41596eaa236
Viktor Lövgren <viktor.lovgren@ovoenergy.com>
2018-02-13 16:24:43 +0000

    Add envF and propF

--- modules/core/shared/src/main/scala/ciris/package.scala
+++ modules/core/shared/src/main/scala/ciris/package.scala
@@ -13,9 +13,14 @@ package object ciris extends LoadConfigs with CirisPlatformSpecific {
   /**
     * Reads the environment variable with the specified key name,
     * and tries to convert the value to type `Value`, wrapping
-    * the result in a [[ConfigEntry]].
+    * the result in a [[ConfigEntry]].<br>
+    * <br>
+    * If you want the value returned in a context, that is not
+    * [[api.Id]], you can use [[envF]] and explicitly specify
+    * the context to use.
     *
     * @param key the name of the environment variable to read
+    * @param decoder the decoder with which to decode the value
     * @tparam Value the type to convert the value to
     * @return a [[ConfigEntry]] with the result
     * @example {{{
@@ -26,17 +31,45 @@ package object ciris extends LoadConfigs with CirisPlatformSpecific {
   def env[Value](key: String)(
     implicit decoder: ConfigDecoder[String, Value]
   ): ConfigEntry[Id, String, String, Value] = {
+    envF[Id, Value](key)
+  }
+
+  /**
+    * Reads the environment variable with the specified key name,
+    * and tries to convert the value to type `Value`, wrapping
+    * the result in a [[ConfigEntry]] where the value has been
+    * lifted into context `F`.<br>
+    * <br>
+    * If no context `F` is desired, [[api.Id]] can be used. There
+    * is also [[env]] which can be used for the case when `F` is
+    * [[api.Id]].
+    *
+    * @param key the name of the environment variable to read
+    * @param decoder the decoder with which to decode the value
+    * @tparam F the context in which to lift the value
+    * @tparam Value the type to convert the value to
+    * @return a [[ConfigEntry]] with the result
+    */
+  def envF[F[_]: Applicative, Value](key: String)(
+    implicit decoder: ConfigDecoder[String, Value]
+  ): ConfigEntry[F, String, String, Value] = {
     ConfigSource.Environment
       .read(key)
       .decodeValue[Value]
+      .transformF[F]
   }
 
   /**
     * Reads the system property with the specified key name,
-    * and tries to convert the value to type `Value`,
-    * wrapping the result in a [[ConfigEntry]].
+    * and tries to convert the value to type `Value`. Note
+    * that system properties are mutable, so if you modify
+    * them during runtime, this function is not pure.<br>
+    * <br>
+    * You might want to use [[propF]] to suspend the actual
+    * reading in a context `F`, like `IO`.
     *
     * @param key the system property to read
+    * @param decoder the decoder with which to decode the value
     * @tparam Value the type to convert the value to
     * @return a [[ConfigEntry]] with the result
     * @example {{{
@@ -53,12 +86,39 @@ package object ciris extends LoadConfigs with CirisPlatformSpecific {
   }
 
   /**
+    * Reads the system property with the specified key name,
+    * but suspends the reading by wrapping the operation in
+    * the specified context `F`, and then tries to convert
+    * the value to type `Value`.<br>
+    * <br>
+    * Note that system properties are mutable. If you never
+    * modify them during runtime, and you are sure that you
+    * do not want to suspend the reading in a context `F`,
+    * you can instead use [[prop]].
+    *
+    * @param key the system property to read
+    * @param decoder the decoder with which to decode the value
+    * @tparam F the context in which to suspend the reading
+    * @tparam Value the type to convert the value to
+    * @return a [[ConfigEntry]] with the result
+    */
+  def propF[F[_]: Sync, Value](key: String)(
+    implicit decoder: ConfigDecoder[String, Value]
+  ): ConfigEntry[F, String, String, Value] = {
+    ConfigSource.Properties
+      .suspendF[F]
+      .read(key)
+      .decodeValue[Value]
+  }
+
+  /**
     * Reads the command-line argument with the specified index,
     * and tries to convert the value to type `Value`, wrapping
     * the result in a [[ConfigEntry]].
     *
-    * @param args the command-line arguments
+    * @param args the immutable command-line arguments
     * @param index the index of the argument to read
+    * @param decoder the decoder with which to decode the value
     * @tparam Value the type to convert the value to
     * @return a [[ConfigEntry]] with the result
     * @example {{{
