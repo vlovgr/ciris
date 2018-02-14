fc0d63a3d372cd428da90663fee04d452c053c3d
Viktor Lövgren <viktor.lovgren@ovoenergy.com>
2018-02-14 09:17:04 +0000

    Update scripts to latest version

--- scripts/try.typelevel.sh
+++ scripts/try.typelevel.sh
@@ -13,19 +13,21 @@ test -e ~/.coursier/coursier || ( \
   org.typelevel:scala-compiler:2.12.4-bin-typelevel-4 \
   org.typelevel:scala-library:2.12.4-bin-typelevel-4 \
   org.typelevel:scala-reflect:2.12.4-bin-typelevel-4 \
-  is.cir:ciris-cats_2.12:0.7.0 \
-  is.cir:ciris-core_2.12:0.7.0 \
-  is.cir:ciris-enumeratum_2.12:0.7.0 \
-  is.cir:ciris-generic_2.12:0.7.0 \
-  is.cir:ciris-refined_2.12:0.7.0 \
-  is.cir:ciris-spire_2.12:0.7.0 \
-  is.cir:ciris-squants_2.12:0.7.0 \
+  is.cir:ciris-cats_2.12:0.7.1 \
+  is.cir:ciris-cats-effect_2.12:0.7.1 \
+  is.cir:ciris-core_2.12:0.7.1 \
+  is.cir:ciris-enumeratum_2.12:0.7.1 \
+  is.cir:ciris-generic_2.12:0.7.1 \
+  is.cir:ciris-refined_2.12:0.7.1 \
+  is.cir:ciris-spire_2.12:0.7.1 \
+  is.cir:ciris-squants_2.12:0.7.1 \
   -- --predef-code "\
         repl.compiler.settings.YliteralTypes.value = true;\
         repl.compiler.settings.YpartialUnification.value = true;\
         import ciris._,\
         ciris.syntax._,\
         ciris.cats._,\
+        ciris.cats.effect._,\
         ciris.enumeratum._,\
         ciris.generic._,\
         ciris.refined._,\
