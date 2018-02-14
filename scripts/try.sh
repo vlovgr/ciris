e62cea61415e53568b1b4b8ac1e1fb76a8aa5e99
Viktor Lövgren <viktor.lovgren@ovoenergy.com>
2018-02-14 11:29:55 +0000

    Update scripts to latest version

--- scripts/try.sh
+++ scripts/try.sh
@@ -7,18 +7,20 @@ test -e ~/.coursier/coursier || ( \
 
 ~/.coursier/coursier launch -q -P \
   com.lihaoyi:ammonite_2.12.4:1.0.3 \
-  is.cir:ciris-cats_2.12:0.7.1 \
-  is.cir:ciris-cats-effect_2.12:0.7.1 \
-  is.cir:ciris-core_2.12:0.7.1 \
-  is.cir:ciris-enumeratum_2.12:0.7.1 \
-  is.cir:ciris-generic_2.12:0.7.1 \
-  is.cir:ciris-refined_2.12:0.7.1 \
-  is.cir:ciris-spire_2.12:0.7.1 \
-  is.cir:ciris-squants_2.12:0.7.1 \
+  is.cir:ciris-cats_2.12:0.7.2 \
+  is.cir:ciris-cats-effect_2.12:0.7.2 \
+  is.cir:ciris-core_2.12:0.7.2 \
+  is.cir:ciris-enumeratum_2.12:0.7.2 \
+  is.cir:ciris-generic_2.12:0.7.2 \
+  is.cir:ciris-refined_2.12:0.7.2 \
+  is.cir:ciris-spire_2.12:0.7.2 \
+  is.cir:ciris-squants_2.12:0.7.2 \
   -- --predef-code "\
         repl.compiler.settings.YpartialUnification.value = true;\
         import ciris._,\
         ciris.syntax._,\
+        ciris.cats._,\
+        ciris.cats.effect._,\
         ciris.enumeratum._,\
         ciris.generic._,\
         ciris.refined._,\
