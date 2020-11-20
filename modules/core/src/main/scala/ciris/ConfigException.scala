ec0d00083783a9a4b3904d34f5f8110488544f3d
Viktor Lovgren <viktor@lovg.ren>
2020-11-20 11:43:10 +0100

    WIP

--- modules/core/src/main/scala/ciris/ConfigException.scala
+++ modules/core/src/main/scala/ciris/ConfigException.scala
@@ -6,77 +6,63 @@
 
 package ciris
 
-import cats.{Eq, Foldable}
+import cats.{Eq, Foldable, Show}
 import cats.implicits._
 
-/**
-  * Exception which occurred while loading or decoding configuration values.
-  */
-sealed abstract class ConfigException(message: String) extends RuntimeException(message) {
+final case class ConfigException(error: ConfigError) extends RuntimeException {
+  final def message: String =
+    ConfigException.message(error)
 
-  /**
-    * Returns the underlying [[ConfigError]] for the exception.
-    */
-  def error: ConfigError
+  override final def getMessage(): String =
+    message
+
+  override final def fillInStackTrace(): Throwable =
+    this
 }
 
-private[ciris] final object ConfigException {
-  final val messageLeading: String =
+final object ConfigException {
+  implicit final val configExceptionEq: Eq[ConfigException] =
+    Eq.by(_.error)
+
+  implicit final val configExceptionShow: Show[ConfigException] =
+    Show.fromToString
+
+  private[ciris] final val messageLeading: String =
     "configuration loading failed with the following errors.\n"
 
-  final val messageTrailing: String =
+  private[ciris] final val messageTrailing: String =
     "\n"
 
-  final val entryLeading: String =
+  private[ciris] final val entryLeading: String =
     "\n  - "
 
-  final val entryTrailing: String =
+  private[ciris] final val entryTrailing: String =
     "."
 
-  final def apply(error: ConfigError): ConfigException = {
-    val message = {
-      val messages =
-        error.messages
+  private[ciris] final def message(error: ConfigError): String = {
+    val messages =
+      error.messages
 
-      val builder =
-        new java.lang.StringBuilder(messageLength(messages))
-          .append(messageLeading)
+    val builder =
+      new java.lang.StringBuilder(messageLength(messages))
+        .append(messageLeading)
 
-      messages.foldLeft(()) { (_, message) =>
-        builder.append(entryLeading).append(message)
-        if (!message.endsWith(entryTrailing))
-          builder.append(entryTrailing)
+    messages.foldLeft(()) { (_, message) =>
+      builder.append(entryLeading).append(message)
+      if (!message.endsWith(entryTrailing))
+        builder.append(entryTrailing)
 
-        ()
-      }
-
-      builder.append(messageTrailing).toString
+      ()
     }
 
-    val _error = error
-    new ConfigException(message) {
-      override final val error: ConfigError =
-        _error
-
-      override final def toString: String =
-        s"ciris.ConfigException: $getMessage"
-    }
+    builder.append(messageTrailing).toString
   }
 
-  final def messageLength[F[_]](
-    messages: F[String]
-  )(implicit F: Foldable[F]): Int = {
+  private[ciris] final def messageLength[F[_]: Foldable](messages: F[String]): Int =
     messages.foldMap { message =>
       message.length + entryLeading.length + {
         if (message.endsWith(entryTrailing)) 0
         else entryTrailing.length
       }
     } + messageLeading.length + messageTrailing.length
-  }
-
-  final def unapply(exception: ConfigException): Some[ConfigError] =
-    Some(exception.error)
-
-  implicit final val configExceptionEq: Eq[ConfigException] =
-    Eq.by(_.error)
 }
