fb31e14b276c5f0118e024e3a300e7d37f93344a
vbergeron <valentin.bergeron@ledger.fr>
2022-09-16 17:11:42 +0200

    More ConfigDecoders for Http4s
    Added the support for ip4s Host and Port as config values (+ tests)
    IP4s is a transitive depndency of Http4s, and those types are required
    to setup server backends.
    
    I also added some lines to the `.gitignore` to help vscode users
    contributing.

--- modules/http4s/src/main/scala/ciris/http4s/http4s.scala
+++ modules/http4s/src/main/scala/ciris/http4s/http4s.scala
@@ -7,8 +7,16 @@
 package ciris
 
 import org.http4s.Uri
+import com.comcast.ip4s.Host
+import com.comcast.ip4s.Port
 
 package object http4s {
   implicit final val uriConfigDecoder: ConfigDecoder[String, Uri] =
     ConfigDecoder[String].mapOption("Uri")(Uri.fromString(_).toOption)
+
+  implicit final val hostConfigDecoder: ConfigDecoder[String, Host] =
+    ConfigDecoder[String].mapOption("Host")(Host.fromString)
+
+  implicit final val portConfigDecoder: ConfigDecoder[String, Port] =
+    ConfigDecoder[String].mapOption("Host")(Port.fromString)
 }
--- modules/http4s/src/test/scala/ciris/http4s/Http4sSpec.scala
+++ modules/http4s/src/test/scala/ciris/http4s/Http4sSpec.scala
@@ -12,6 +12,7 @@ import ciris._
 import org.http4s.syntax.literals._
 import org.http4s.Uri
 import org.scalatest.funsuite.AnyFunSuite
+import com.comcast.ip4s._
 
 final class Http4sSpec extends AnyFunSuite {
   test("uriConfigDecoder.success") {
@@ -26,5 +27,48 @@ final class Http4sSpec extends AnyFunSuite {
     assert {
       default("invalid uri").as[Uri].attempt[IO].unsafeRunSync().isLeft
     }
+  }
+
+  test("hostConfigDecoder.success") {
+    assert {
+      val actual = default("localhost").as[Host].attempt[IO].unsafeRunSync()
+      val expected = Right(host"localhost")
+      actual === expected
+    }
+
+    assert {
+      val actual = default("0.0.0.0").as[Host].attempt[IO].unsafeRunSync()
+      val expected = Right(ip"0.0.0.0")
+      actual === expected
+    }
+
+    assert {
+      val actual = default("::1").as[Host].attempt[IO].unsafeRunSync()
+      val expected = Right(ip"::1")
+      actual === expected
+    }
+
+  }
+
+  test("hostConfigDecoder.error") {
+    default("🍋.🍊.🍉.🍐").as[Host].attempt[IO].unsafeRunSync().isLeft
+  }
+
+  test("portConfigDecoder.success") {
+
+    assert {
+      val actual = default("12345").as[Port].attempt[IO].unsafeRunSync()
+      val expected = Right(port"12345")
+      actual === expected
+    }
+
+  }
+
+  test("portConfigDecoder.error") {
+
+    assert {
+      default("🚨").as[Port].attempt[IO].unsafeRunSync().isLeft
+    }
+
   }
 }
