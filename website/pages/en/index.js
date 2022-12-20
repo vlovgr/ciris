62db7afb6d3eee77ba37237988c70ed074900882
Viktor Rudebeck <1163201+vlovgr@users.noreply.github.com>
2022-12-20 14:47:27 +0100

    Fix github actions status badge

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -98,7 +98,7 @@ class Index extends React.Component {
     );
 
     const index =
-      `[![Typelevel](https://img.shields.io/badge/typelevel-library-fd3d50.svg)](https://typelevel.org/projects/#ciris) [![GitHub Actions](https://img.shields.io/github/actions/workflow/status/vlovgr/ciris/ci.yml?branch=master) [![Discord](https://img.shields.io/discord/632277896739946517?color=5865f2)](https://discord.gg/XF3CXcMzqD) [![Version](https://img.shields.io/badge/version-v${latestVersionBadge}-orange.svg)](https://index.scala-lang.org/vlovgr/ciris)
+      `[![Typelevel](https://img.shields.io/badge/typelevel-library-fd3d50.svg)](https://typelevel.org/projects/#ciris) [![GitHub Actions](https://img.shields.io/github/actions/workflow/status/vlovgr/ciris/ci.yml?branch=master)](https://github.com/vlovgr/ciris/actions) [![Discord](https://img.shields.io/discord/632277896739946517?color=5865f2)](https://discord.gg/XF3CXcMzqD) [![Version](https://img.shields.io/badge/version-v${latestVersionBadge}-orange.svg)](https://index.scala-lang.org/vlovgr/ciris)
 
 Functional, lightweight, and composable configuration loading for Scala.<br>
 Project is under active development. Feedback and contributions welcome.
