1bd493968c06ed7cd9bd5c7ed51916bc56c2be9d
Raphaël Fromentin <rafbodaha@gmail.com>
2024-08-27 16:50:47 +0200

    feat: Add codecs for squants

--- modules/squants/shared/src/main/scala/ciris/squants/squants.scala
+++ modules/squants/shared/src/main/scala/ciris/squants/squants.scala
@@ -9,8 +9,15 @@ package ciris
 import _root_.squants.{Dimension, Quantity}
 
 package object squants {
+  
+  @deprecated("Use ConfigCodec instead", "3.7.0")
   implicit final def stringQuantityConfigDecoder[A <: Quantity[A]](
     implicit dimension: Dimension[A]
   ): ConfigDecoder[String, A] =
     ConfigDecoder[String].mapOption(dimension.name) { s => dimension.parseString(s).toOption }
+
+  implicit final def stringQuantityConfigCodec[A <: Quantity[A]](
+    implicit dimension: Dimension[A]
+  ): ConfigCodec[String, A] =
+    ConfigCodec[String].imapOption(dimension.name)(s => dimension.parseString(s).toOption)(_.toString())
 }
