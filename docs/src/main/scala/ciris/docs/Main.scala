7f6d9458f1f0ab6d8b127fb30687687efefcc02d
Viktor Rudebeck <viktor@rudebeck.nu>
2025-08-18 11:22:28 +0200

    Add scala.js support for ciris-http4s-aws

--- docs/src/main/scala/ciris/docs/Main.scala
+++ docs/src/main/scala/ciris/docs/Main.scala
@@ -82,6 +82,7 @@ object Main {
           "HTTP4S_NATIVE_CROSS_SCALA_VERSIONS" -> scalaVersionsString(http4sNativeCrossScalaVersions),
           "HTTP4S_VERSION" -> http4sVersion,
           "HTTP4SAWS_CROSS_SCALA_VERSIONS" -> scalaVersionsString(http4sAwsCrossScalaVersions),
+          "HTTP4SAWS_JS_CROSS_SCALA_VERSIONS" -> scalaVersionsString(http4sAwsJsCrossScalaVersions),
           "HTTP4SAWS_MODULE_NAME" -> http4sAwsModuleName,
           "HTTP4SAWS_VERSION" -> http4sAwsVersion,
           "LATEST_MAJOR_VERSION" -> majorVersion(latestVersion),
